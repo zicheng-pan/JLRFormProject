4a2a2987d1f4f3176fb9fdb162aa38a884c97837
tshi1 <tshi1@jlr1cnxmjxl97.shanghai.jlrint.com>
2022-12-12 10:12:58 +0800

    commit

--- src/main/java/com/example/jlrform/controller/UserController.java
+++ src/main/java/com/example/jlrform/controller/UserController.java
@@ -1,4 +1,26 @@
-package com.example.jlrform.controller;/**
+package com.example.jlrform.controller;
+
+import com.example.jlrform.entity.User;
+import com.example.jlrform.service.UserService;
+import javax.xml.ws.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @author tshi1
- */public class UserController {
+ */
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/user/all")
+    public Response<User> setCountDownPageNumber() {
+        Response<User> response = new Response<>();
+
+        return new Response<>(userService.getAllUsers());
+    }
 }

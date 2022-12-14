352c9806278a9131008ee0711f96af7fa4e4e7f5
tshi1 <tshi1@jlr1cnxmjxl97.shanghai.jlrint.com>
2022-12-14 14:52:51 +0800

    change host to ip

--- src/main/resources/static/js/hostname.js
+++ src/main/resources/static/js/hostname.js
@@ -1,3 +1,3 @@
 // var serverhost = "http://zicheng.info:8080";
-var serverhost = "https://exam.cross-env.cloudcar.oneiotworld.com"
+var serverhost = "http://52.82.122.181:8080"
 // var serverhost = "http://localhost:8080";

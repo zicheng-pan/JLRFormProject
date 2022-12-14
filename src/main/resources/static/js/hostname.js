c34b2274402750d95d9b93fe4ec24cda60b9a582
zicheng-pan <zpan2@jaguarlandrover.com>
2022-12-14 17:15:17 +0800

    小改

--- src/main/resources/static/js/hostname.js
+++ src/main/resources/static/js/hostname.js
@@ -1,3 +1,3 @@
 // var serverhost = "http://zicheng.info:8080";
-// var serverhost = "http://52.82.122.181:8080"
-var serverhost = "http://localhost:8080";
+var serverhost = "http://52.82.122.181:8080"
+// var serverhost = "http://localhost:8080";

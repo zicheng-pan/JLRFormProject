8a9cca6ede4d31be782192117a21ea2311108166
zicheng-pan <zpan2@jaguarlandrover.com>
2022-12-15 15:01:09 +0800

    添加中文字符返回

--- src/main/java/com/example/jlrform/controller/UserController.java
+++ src/main/java/com/example/jlrform/controller/UserController.java
@@ -3,7 +3,9 @@ package com.example.jlrform.controller;
 import com.example.jlrform.dto.UserDto;
 import com.example.jlrform.entity.User;
 import com.example.jlrform.service.UserService;
+
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,33 +21,33 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/user/all")
+    @GetMapping(value = "/user/all", produces = "application/json; charset=utf-8")
     public List<User> getAllUsers() {
 
         return userService.getAllUsers();
     }
 
-    @GetMapping("/user/cdsid")
+    @GetMapping(value = "/user/cdsid", produces = "application/json; charset=utf-8")
     public User getUserByCdsid(@RequestParam String cdsid) {
         return userService.getUserByCDSId(cdsid);
     }
 
-    @PostMapping("/user")
+    @PostMapping(value = "/user", produces = "application/json; charset=utf-8")
     public User saveUser(@RequestParam String eName
-            ,@RequestParam String cName
-            ,@RequestParam String cdsid
-            ,@RequestParam Boolean involved
-            ,@RequestParam int score) {
+            , @RequestParam String cName
+            , @RequestParam String cdsid
+            , @RequestParam Boolean involved
+            , @RequestParam int score) {
 
-        return userService.saveUser(cdsid,eName,cName,involved,score);
+        return userService.saveUser(cdsid, eName, cName, involved, score);
     }
 
-    @GetMapping("/user/rank")
+    @GetMapping(value = "/user/rank", produces = "application/json; charset=utf-8")
     public List<UserDto> getRank() {
         return userService.getAllRank();
     }
 
-    @GetMapping("/user/priceRank")
+    @GetMapping(value = "/user/priceRank", produces = "application/json; charset=utf-8")
     public List<UserDto> getPriceRank() {
         return userService.getPriceRank();
     }

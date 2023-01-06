60033f65ac5deff22f17a85ff464ba384611ebbd
zicheng-pan <zpan2@jaguarlandrover.com>
2023-01-06 15:35:29 +0800

    添加设置缓存头的拦截配置

---
+++ src/main/java/com/example/jlrform/config/WebConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.jlrform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(2592000, TimeUnit.SECONDS));
+    }
+
+
+}

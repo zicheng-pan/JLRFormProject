e7075cd1fc41a2d1b9703aa6baa1864cd01d0192
zicheng-pan <zpan2@jaguarlandrover.com>
2023-01-09 15:46:20 +0800

    修改host

--- src/main/resources/static/js/hostname.js
+++ src/main/resources/static/js/hostname.js
@@ -1,5 +1,5 @@
 // var serverhost = "http://zicheng.info:8080";
-// var serverhost = "https://exam.cross-env.cloudcar.oneiotworld.com";
+var serverhostname = "https://exam.cross-env.cloudcar.oneiotworld.com";
 var serverhost = "http://69.230.253.37:8080";
 // var serverhost = "http://localhost:8080";
 // var serverhost = "http://192.168.31.213:8080";

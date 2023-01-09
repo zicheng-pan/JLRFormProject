7f7f3648f3baee0b019410a562cc04e5c409a614
zicheng-pan <zpan2@jaguarlandrover.com>
2023-01-09 14:44:01 +0800

    修改host和二维码图片

--- src/main/resources/static/js/hostname.js
+++ src/main/resources/static/js/hostname.js
@@ -1,4 +1,4 @@
 // var serverhost = "http://zicheng.info:8080";
-var serverhost = "http://69.230.253.37:8080"
+var serverhost = "https://exam.cross-env.cloudcar.oneiotworld.com";
 // var serverhost = "http://localhost:8080";
 // var serverhost = "http://192.168.31.213:8080";
